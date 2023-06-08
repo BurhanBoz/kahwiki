2be9bcaf1f2752a335f3fb1c0c768550a4b2fa35
BurhanBoz <boz.burhan8@gmail.com>
2023-06-08 20:32:50 +0300

    initial commit

---
+++ src/main/java/com/example/kahwiki/KahwikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kahwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KahwikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kahwiki/KahwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kahwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahwikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
